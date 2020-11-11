7b10c9f983c93933569f38fe94ec2c3f58416e49
afaist <afaist@gmail.com>
2020-11-11 16:21:57 +0700

    4.3.5 Отрицательные проверки: как проверить отсутствие элемента

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
 import pyperclip
 
@@ -23,11 +26,11 @@ class BasePage():
         два аргумента:
             как искать (css, id, xpath и тд)
             и собственно что искать (строку-селектор)
-        """
-        try:
-            self.browser.find_element(how, what)
-        except NoSuchElementException:
-            return False
+            """
+            try:
+                self.browser.find_element(how, what)
+            except NoSuchElementException:
+                return False
         return True
 
 
@@ -56,3 +59,26 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
+
+    def is_not_element_present(self, how, what, timeout=4):
+        """
+        Проверяет, что элемент не появляется на странице в течение
+        заданного времени
+        """
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+
+    def is_disappeared(self, how, what, timeout=4):
+        """
+        Проверяет, что какой-то элемент исчезает
+        """
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
